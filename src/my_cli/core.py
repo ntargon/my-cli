6e4a40d23e95379d83f172810dc3fb0151dea52c
ntargon <argon19951128@gmail.com>
2024-11-29 23:42:22 +0900

    feat: add flake8, balck, isort, mypy

--- src/my_cli/core.py
+++ src/my_cli/core.py
@@ -1,9 +1,10 @@
-
 def add(x: int, y: int) -> int:
     return x + y
 
-def main():
-    print('main')
 
-if __name__ == '__main__':
+def main():
+    print("main")
+
+
+if __name__ == "__main__":
     main()
